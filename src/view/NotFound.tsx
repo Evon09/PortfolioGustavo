788b035a72316079f708ad7e03f50e08922cadfd
JairJS <jair.sequeira@siliconvillage.dev>
2024-01-30 18:21:30 -0400

    feat: pagina 404

--- src/view/NotFound.tsx
+++ src/view/NotFound.tsx
@@ -1,16 +1,44 @@
-import { Center, Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Flex,
+  Link,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Header } from "../components/commons/Header";
 import Footer from "../components/commons/footer";
 
 export default function NotFound() {
+  const { colorMode } = useColorMode();
   return (
     <Flex direction="column" h="100vh" align="center" justify="space-between">
       <Header />
 
-      <Center h="100%" bg="gray.600" w="100%">
-        <Flex direction="column" align="center">
-          <Text fontSize="150px">404</Text>
-          <Text fontSize="100px">Não Localizdo</Text>
+      <Center
+        h="100%"
+        bg={colorMode === "light" ? "light.background" : "dark.background"}
+        w="100%"
+        style={{ fontFamily: "Lato, serif" }}
+      >
+        <Flex
+          direction="column"
+          align="center"
+          w="70v"
+          textAlign="center"
+          gap="20px"
+        >
+          <Text fontSize="150px">404!</Text>
+
+          <Text fontSize="70px">Parece que você está perdido no roteiro!</Text>
+          <Text w="50vw" fontSize="20px">
+            Parece que houve um problema de edição e você acabou em uma cena
+            deletada. Nossos editores estão trabalhando para encontrar o seu
+            caminho de volta à trama principal.
+          </Text>
+          <Link href="/">
+            <Button>Voltar!</Button>
+          </Link>
         </Flex>
       </Center>
       <Footer></Footer>
